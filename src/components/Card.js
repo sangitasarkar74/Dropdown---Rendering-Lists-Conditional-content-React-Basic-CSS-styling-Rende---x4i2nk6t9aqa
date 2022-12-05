674aa3d7ac10ca04fb44bfb891354ad0eb2f655b
sangitasarkar74 <sangitasarkar@gmail.com>
2022-12-05 16:27:06 +0530

    first stage

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+const Card = ({ title, name, description }) => {
+    return (
+        <article style={{
+            border: "1px solid #c1c1c1",
+            padding: "0.5rem",
+            margin: "0.5rem 0"
+        }}
+            id={`${title}-title`}
+        >
+            <h3 id={`${title}-name`}>{name}</h3>
+            <p id={`${title}-description`}>{description}  </p>
+        </article>
+    )
+}
+
+export default Card;
